5af2dabd770919d4d8eee5b2ec11cae726bdcbbb
Heaveness <vindictuslbp@gmail.com>
2023-07-31 20:30:54 -0400

    Added the ability for user to input username or email address to log in and reflect those changes in related files. Also fixed an mistake of not asking user to enter their email address to sign-up.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
+const { Op } = require('sequelize');
 const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // Create a new user
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const userData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
 
     req.session.save(() => {
       req.session.userId = userData.id;
@@ -23,17 +28,24 @@ router.post('/', async (req, res) => {
 // User login
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({
+      where: {
+        [Op.or]: [
+          { email: req.body.identifier },
+          { username: req.body.identifier }
+        ]
+      }
+    });
 
     if (!userData) {
-      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      res.status(400).json({ message: 'Incorrect email or username, please try again' });
       return;
     }
 
     const validPassword = await userData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      res.status(400).json({ message: 'Incorrect password, please try again' });
       return;
     }
 
--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,25 @@
 const loginFormHandler = async (event) => {
-  event.preventDefault();
-
-  const email = document.querySelector('#email').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
-
-  if (email && password) {
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      document.getElementById('login-error').classList.remove('d-none');
+    event.preventDefault();
+  
+    const identifier = document.querySelector('#identifier').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (identifier && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ identifier, password }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
     }
-  }
-};
-
+  };
+  
 document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+
 
   
