89e9f21f28b5dfa7a8d5b3f38da9a04b17aeaa73
Heaveness <vindictuslbp@gmail.com>
2023-07-31 20:22:20 -0400

    Added Posted By Username on Format Date feature on the posts, and an error message if user inputs incorrect credentials.

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,24 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      document.getElementById('login-error').classList.remove('d-none');
     }
-  };
-  
-  document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+  }
+};
+
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+
   
