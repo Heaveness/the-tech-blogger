282f5498c870b4328d9a252e56ea8e0fc96df8e5
Heaveness <vindictuslbp@gmail.com>
2023-08-02 16:15:23 -0400

    Added a fix to be deployed into Heroku as I forgot to add a way to host without localhost.

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,29 @@
 // Import the required packages and libraries.
 require('dotenv').config();  // Load environment variables from a .env file into process.env.
 const Sequelize = require('sequelize');  // Import Sequelize library.
+const url = require('url');
 
-// Create a new Sequelize instance with the appropriate configuration.
+let sequelize;
+
 // The configuration details are loaded from environment variables.
-const sequelize = new Sequelize(
-  process.env.DB_NAME,  // Name of the database.
-  process.env.DB_USER,  // User name for database.
-  process.env.DB_PASSWORD,  // Password for database user.
-  {
-    host: process.env.DB_HOST || 'localhost',  // Database host.
-    port: process.env.DB_PORT || 3306,  // Database port.
-    dialect: 'mysql',  // Database dialect.
-    dialectOptions: {
-      decimalNumbers: true,  // Enable support for decimal numbers.
-    },
-    logging: false, // Set to true if you want to see SQL queries in the console.
-  }
-);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,  // Name of the database.
+    process.env.DB_USER,  // User name for database.
+    process.env.DB_PASSWORD,  // Password for database user.
+    {
+      host: process.env.DB_HOST || 'localhost',  // Database host.
+      port: process.env.DB_PORT || 3306,  // Database port.
+      dialect: 'mysql',  // Database dialect.
+      dialectOptions: {
+        decimalNumbers: true,  // Enable support for decimal numbers.
+      },
+      logging: false, // Set to true if you want to see SQL queries in the console.
+    }
+  )
+};
 
 // Export the sequelize instance to be used in other parts of the application.
 module.exports = sequelize;
